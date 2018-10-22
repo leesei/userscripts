1996d2b13db8f7a5377bd381270237947ea2505d
leesei <leesei@gmail.com>
2018-10-22 11:29:52 +0800

    fix formatting

--- clean-url-query/clean-url-query.user.js
+++ clean-url-query/clean-url-query.user.js
@@ -14,30 +14,35 @@
 // ==/UserScript==
 
 function log(level, text) {
-    GM_log(level + ": " + text);
+  GM_log(level + ": " + text);
 }
 
 function query2json(querystring) {
-    // remove any preceding url and split
-    var queries = querystring.substring(querystring.indexOf('?')+1).split('&');
-    var params = {}, pair, d = decodeURIComponent;
-    // march and parse
-    for (var i = queries.length - 1; i >= 0; i--) {
-        pair = queries[i].split('=');
-        params[d(pair[0])] = d(pair[1]);
-    }
+  // remove any preceding url and split
+  var queries = querystring.substring(querystring.indexOf("?") + 1).split("&");
+  var params = {},
+    pair,
+    d = decodeURIComponent;
+  // march and parse
+  for (var i = queries.length - 1; i >= 0; i--) {
+    pair = queries[i].split("=");
+    params[d(pair[0])] = d(pair[1]);
+  }
 
-    return params;
+  return params;
 }
 
 (function() {
-    'use strict';
+  "use strict";
 
-    log("info", ">>> [" + GM_info.script.namespace + "] " + GM_info.script.name + " <<<");
+  log(
+    "info",
+    ">>> [" + GM_info.script.namespace + "] " + GM_info.script.name + " <<<"
+  );
 
-    var queries = query2json(location.search)
-    log('debug', JSON.stringify(queries))
-    if (Object.keys(queries).length > 1) {
-        location.replace(location.pathname + '?id=' + queries.id)
-    }
+  var queries = query2json(location.search);
+  log("debug", JSON.stringify(queries));
+  if (Object.keys(queries).length > 1) {
+    location.replace(location.pathname + "?id=" + queries.id);
+  }
 })();
