94cd657782291ca0e9274653dcbd1da5e9314262
leesei <leesei@gmail.com>
2018-10-24 15:22:16 +0800

    add `_skip_clean`, also retain `page` param

--- clean-url-query/clean-url-query.user.js
+++ clean-url-query/clean-url-query.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Clean URL Query
 // @namespace    https://github.com/leesei/userscripts
-// @version      1.1
+// @version      1.3.1
 // @description  Trim tracking query params from URL
 // @author       leesei@gmail.com
 // @supportURL   https://github.com/leesei/userscripts/issues
@@ -10,6 +10,7 @@
 // @match        http*://item.taobao.com/item.htm*
 // @match        http*://*.taobao.com/shop/view_shop.htm
 // @match        http*://*.computerworld.com/*
+// @match        http*://*.networkworld.com/*
 // @match        http*://*.infoworld.com/*
 // @run-at       document-start
 // @grant        GM_log
@@ -27,13 +28,46 @@ function query2json(querystring) {
   var params = {},
     pair,
     d = decodeURIComponent;
-  // march and parse
+  // match and parse
   for (var i = queries.length - 1; i >= 0; i--) {
+    if (queries[i].length === 0) continue;
     pair = queries[i].split("=");
     params[d(pair[0])] = d(pair[1]);
   }
 
   return params;
+}
+
+function json2query(json) {
+  var query = Object.keys(json)
+    .map(function(key) {
+      return encodeURIComponent(key) + "=" + encodeURIComponent(json[key]);
+    })
+    .join("&");
+
+  return query ? "?" + query : "";
+}
+
+// Convert sensible strings to Boolean, useful for parsing URL queries
+function string2Boolean(string, defaultTrue) {
+  // console.log('2bool:', String(string).toLowerCase());
+  switch (String(string).toLowerCase()) {
+    case "":
+      return defaultTrue === undefined ? false : defaultTrue;
+    case "true":
+    case "1":
+    case "yes":
+    case "y":
+      return true;
+    case "false":
+    case "0":
+    case "no":
+    case "n":
+      return false;
+    default:
+      // you could throw an error, but 'undefined' seems a more logical value
+      return undefined;
+  }
 }
 
 (function() {
@@ -45,11 +79,31 @@ function query2json(querystring) {
   );
 
   var queries = query2json(location.search);
-  log("debug", JSON.stringify(queries));
-  if (Object.keys(queries).length > 1) {
-    location.replace(
-      // retain id for Taobao items
-      location.pathname + (queries.id ? "?id=" + queries.id : "")
-    );
+  log("debug", "queries: " + JSON.stringify(queries));
+
+  if (
+    queries._skip_clean !== undefined &&
+    string2Boolean(queries._skip_clean)
+  ) {
+    return;
+  }
+
+  // retain these query params
+  var WHITE_LIST = [
+    "id", // for taobao items
+    "page" // for article pages
+  ];
+  var copy = Object.assign({}, queries);
+  WHITE_LIST.forEach(key => delete copy[key]);
+  log("debug", "copy: " + JSON.stringify(copy));
+
+  // remove non-whitelisted queries
+  if (Object.keys(copy).length) {
+    var _q = {};
+    WHITE_LIST.forEach(key => {
+      if (queries[key]) _q[key] = queries[key];
+    });
+    log("debug", "_q: " + JSON.stringify(_q));
+    location.replace(location.pathname + json2query(_q));
   }
 })();
