621a6769c9af66800f851797fb23bfb0da136a46
leesei <leesei@gmail.com>
2023-04-19 17:32:09 +0800

    [single-page-everything] revamp script

--- single-page-everything/README.md
+++ single-page-everything/README.md
@@ -5,23 +5,22 @@ This user script tries to load the single page version (if available).
 
 Contributions are welcome.
 
+[HowStuffWorks - Learn How Everything Works!](https://www.howstuffworks.com/)  
+Lookup "Print" link for multi-page articles.
+
 [AnandTech: Hardware News and Tech Reviews Since 1997](http://www.anandtech.com/)  
 Replace `/show/` to `/print/` in url for single page layout.
 
-[HowStuffWorks - Learn How Everything Works!](https://www.howstuffworks.com/)  
-Append `/printable` in url for single page layout.
+~~[Tom’s Hardware: For The Hardcore PC Enthusiast](https://www.tomshardware.com/)~~
 
-[Tom’s Hardware: For The Hardcore PC Enthusiast](https://www.tomshardware.com/)
-Replace `/reviews/` to `/print/` and add `review` after `,` in url for single page layout.
+[Adafruit Learning System](https://learn.adafruit.com/)  
+Lookup "Single page" link for multi-page articles.
 
-[Adafruit Learning System](https://learn.adafruit.com/)
-Append `?view=all` in url for single page layout.
+[Learn at SparkFun Electronics - learn.sparkfun.com](https://learn.sparkfun.com/)  
+Lookup "Single Page" link for multi-page articles.
 
-[Learn at SparkFun Electronics - learn.sparkfun.com](https://learn.sparkfun.com/)
-Append `/all` in url for single page layout.
-
-[Ars Technica](https://arstechnica.com/)
-Append `?amp=1` in url for single page layout.
+[Ars Technica](https://arstechnica.com/)  
+Lookup "amphtml" link for multi-page articles.
 
 ## Pending
 
@@ -32,6 +31,6 @@ Append `?amp=1` in url for single page layout.
 
 ## Similar Projects
 
-[Tom's Hardware Print View](https://greasyfork.org/en/scripts/34630-tom-s-hardware-print-view)
-[Printer Friendly Redirect for Greasemonkey](http://userscripts-mirror.org/scripts/show/7986)
+[Tom's Hardware Print View](https://greasyfork.org/en/scripts/34630-tom-s-hardware-print-view)  
+[Printer Friendly Redirect for Greasemonkey](http://userscripts-mirror.org/scripts/show/7986)  
 [AutoPrint 4.2 for Greasemonkey](http://userscripts-mirror.org/scripts/show/13368)
--- single-page-everything/single-page-everything.user.js
+++ single-page-everything/single-page-everything.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Single Page Everything
-// @namespace    https://github.com/leesei/userscripts
-// @version      1.4.1
+// @namespace    @leesei/userscripts
+// @version      2.0.0
 // @description  Load single page version of page for supported sites
 // @author       leesei@gmail.com
 // @supportURL   https://github.com/leesei/userscripts/issues
-// @match        http*://*.howstuffworks.com/*.htm
+// @match        http*://*.howstuffworks.com/*
 // @match        http*://www.anandtech.com/show/*
-// match        http*://www.tomshardware.com/reviews/*.html
+/// @match        http*://www.tomshardware.com/reviews/*.html
 // @match        http*://learn.adafruit.com/*
 // @match        http*://learn.sparkfun.com/tutorials/*
 // @match        http*://arstechnica.com/*
@@ -21,22 +21,50 @@ function log(level, text) {
   GM_log(level + ": " + text);
 }
 
-function query2json(querystring) {
-  // remove any preceding url and split
-  var queries = querystring.substring(querystring.indexOf("?") + 1).split("&");
-  var params = {},
-    pair,
-    d = decodeURIComponent;
-  // march and parse
-  for (var i = queries.length - 1; i >= 0; i--) {
-    pair = queries[i].split("=");
-    params[d(pair[0])] = d(pair[1]);
-  }
+const handlers = {
+  howstuffworks: function (params) {
+    if (!location.pathname.endsWith(".htm")) return;
 
-  return params;
-}
+    const links = document.querySelectorAll("a[rel='nofollow']");
+    for (const link of links) {
+      if (link.innerText == "Print") {
+        return location.replace(link.href);
+      }
+    }
+  },
+  anandtech: function (params) {
+    location.replace(location.pathname.replace("/show", "/print"));
+  },
+  // tomshardware: function (params) {
+  //   location.replace(
+  //     location.pathname.replace("/reviews", "/print").replace(",", ",reviews-")
+  //   );
+  // },
+  adafruit: function (params) {
+    const links = document.querySelectorAll(
+      "ul[aria-label='Guide resources'] li a"
+    );
+    for (const link of links) {
+      if (link.innerText == "Single page") {
+        return location.replace(link.href);
+      }
+    }
+  },
+  sparkfun: function (params) {
+    const links = document.querySelectorAll("a.list-group-item");
+    for (const link of links) {
+      if (link.innerText == "Single Page") {
+        return location.replace(link.href);
+      }
+    }
+  },
+  arstechnica: function (params) {
+    const link = document.querySelector("link[rel='amphtml']");
+    if (link) location.replace(link.href);
+  },
+};
 
-(function() {
+(function () {
   "use strict";
 
   log(
@@ -44,34 +72,13 @@ function query2json(querystring) {
     ">>> [" + GM_info.script.namespace + "] " + GM_info.script.name + " <<<"
   );
 
-  var queries = query2json(location.search);
+  const params = new URLSearchParams(location.search);
+  // log("info", JSON.stringify(params.entries()));
 
-  if (location.hostname.includes("howstuffworks")) {
-    location.replace(location.pathname, location.pathname + "/printable");
-  } else if (location.hostname.includes("anandtech")) {
-    location.replace(location.pathname.replace("/show", "/print"));
-    //   } else if (location.hostname.includes("tomshardware")) {
-    //     location.replace(
-    //       location.pathname.replace("/reviews", "/print").replace(",", ",reviews-")
-    //     );
-  } else if (location.hostname.includes("adafruit")) {
-    // only handle articles, excludes category
-    if (location.pathname.includes("/category/", 1)) return;
-
-    if (!queries.view) {
-      location.replace(location.pathname + "?view=all");
-    }
-  } else if (location.hostname.includes("sparkfun")) {
-    // already single page layout
-    if (location.pathname.includes("/all", 1)) return;
-
-    location.replace(location.pathname + "/all");
-  } else if (location.hostname.includes("arstechnica")) {
-    // only handle articles, this may be category
-    if (!location.pathname.includes("/", 1)) return;
-
-    if (!queries.amp) {
-      location.replace(location.pathname + "?amp=1");
+  for (const domain in handlers) {
+    if (location.hostname.includes(domain)) {
+      handlers[domain](params);
+      break;
     }
   }
 })();
